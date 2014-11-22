5cadb853fdfb7c4ef3830f23b4a7222809a6a952
Rav Mahov <ravmahov@gmail.com>
2014-11-22 12:58:22 +0100

    added: algorithm that uses utils.h

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
 int main()
 {
+	int tab* = random();
+	display( tab, 10 );
+	cout << min( tab, 10 ) << " " << max( tab, 10 ) << " " << sum( tab, 10 )<< endl;
 	return 0;
 }
