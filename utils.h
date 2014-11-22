b9b08846ff05a6a5803cd7bab24660d056949d8b
Marta Kondratowicz <marta.kondratowicz.1993@gmail.com>
2014-11-22 12:59:02 +0100

    Usunięto pliki generator, max, min .h, a ich zawartość przeniesiono do utils.h

--- utils.h
+++ utils.h
@@ -16,3 +16,33 @@ int sum (int[] t, int n)
 	}
 	return sum;
 }
+int min( int* arr, int len)
+{
+	int min = arr[0];
+	for( int i= 1;  i< len; i++ )
+	{
+		if( min > arr[i] )
+			min = arr[i];
+	}
+	return min;
+}
+int max( int[] t, int n)
+{
+	int max=t[0];
+	for(int i=1;i<n;i++)
+	{
+		if(t[i]>t[0])
+		{
+			max=t[i];
+		}
+	}
+	return max;
+}
+int[] random()
+{
+	int [] tab=new tab[10];
+	srand( time( NULL ) );
+    for( int i = 0; i < 10; i++ )
+         tab[i]=rand()%100 + 1;
+	return tab;
+}
